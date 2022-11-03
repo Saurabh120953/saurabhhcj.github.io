5909f7f4b28bdc9b1ff85e737399eed1bfdd81de
saurabh yadav <110917233+Saurabh120953@users.noreply.github.com>
2022-11-03 21:19:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = " hello world  ";
+
+<button>Change user</button>
+
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `Mozilla is cool, ${myName}`;
+    }
+  }
+  myButton.onclick = () => {
+    setUserName();
+  };
+  function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `Mozilla is cool, ${myName}`;
+    }
+  }
+  function setUserName() {
+    const myName = prompt("Please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+  
+
+
+
+
+
+
+
+
+
+myVariable = "Bob";
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/firefox2.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+};
+
+<button>Change user</button>
+let myButton = document.querySelector("
